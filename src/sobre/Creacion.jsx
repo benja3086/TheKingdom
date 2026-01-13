4a7c81fd288470969d31a564da36e7e9e3108176
benja3086 <benjaminquinteros785@gmail.com>
2026-01-13 05:47:04 -0300

    cambiada la ruta de las fotos2

--- src/sobre/Creacion.jsx
+++ src/sobre/Creacion.jsx
@@ -4,10 +4,10 @@ import './Creacion.css';
 const Creacion = () => {
   return (
     <div className="galeria">
-      <img src="public\lacuenta2.jpg" alt="Creacion de Nosotros" />
-      <img src="public\flores.jpg" alt="Creacion de Nosotros" />
-      <img src="public\corazon-roto2.jpg" alt="Creacion de Nosotros" />
-      <img src="public\navidad.jpg" alt="Creacion de Nosotros" />
+      <img src="public/lacuenta2.jpg" alt="Creacion de Nosotros" />
+      <img src="public/flores.jpg" alt="Creacion de Nosotros" />
+      <img src="public/corazon-roto2.jpg" alt="Creacion de Nosotros" />
+      <img src="public/navidad.jpg" alt="Creacion de Nosotros" />
     </div>
   );
 };
